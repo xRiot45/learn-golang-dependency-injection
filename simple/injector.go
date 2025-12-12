ab125b4928ffdcecc6020531d67d85e90a3adce5
rumahkodingku <rumahkodingku45@gmail.com>
2025-12-12 11:23:08 +0700

    struct provider

--- simple/injector.go
+++ simple/injector.go
@@ -47,3 +47,8 @@ func InitializedHelloService() *HelloService {
 	wire.Build(helloSet, NewHelloService)
 	return nil
 }
+
+func InitializedFooBar() *FooBar {
+	wire.Build(NewFoo, NewBar, wire.Struct(new(FooBar), "Foo", "Bar"))
+	return nil
+}
